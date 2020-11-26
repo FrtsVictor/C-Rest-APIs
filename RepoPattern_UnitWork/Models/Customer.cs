de1cc0f632cceaedf753da8d470a6df1c2d75e64
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-26 15:20:11 -0300

    Add Customers & Orders /Models

---
+++ RepoPattern_UnitWork/Models/Customer.cs
@@ -0,0 +1,8 @@
+namespace RepoPattern_UnitWork.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ RepoPattern_UnitWork/Models/Order.cs
@@ -0,0 +1,11 @@
+namespace RepoPattern_UnitWork.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+    }
+}

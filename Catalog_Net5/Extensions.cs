fe28469d9cb11df4accccac3275a528859fca2bf
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-27 09:59:46 -0300

    Add Extensions for Dto /root

---
+++ Catalog_Net5/Extensions.cs
@@ -0,0 +1,19 @@
+using Catalog_Net5.Dtos;
+using Catalog_Net5.Entities;
+
+namespace Catalog_Net5
+{
+    public static class Extensions
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Price = item.Price,
+                CreatedDate = item.CreatedDate
+            };
+        }
+    }
+}

78c4efcd2540f675f8b900f0bd70d19e207d1950
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-27 09:01:37 -0300

    Add RecordItem /Entities

---
+++ Catalog_Net5/Entities/Item.cs
@@ -0,0 +1,27 @@
+using System.Runtime.CompilerServices;
+using System;
+
+namespace Catalog_Net5.Entities
+{
+    public record Item
+    {
+        public Guid Id { get; init; }  //setting init for down allow modifications across the application
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+        public DateTimeOffset CreatedDate { get; set; }
+
+    }
+}
+
+
+
+
+
+//can do
+// Item item = new()
+// {
+// Id = Guid.NewGuid();
+// }
+
+//can't do
+// Item.Id = Guid.NewGuid()

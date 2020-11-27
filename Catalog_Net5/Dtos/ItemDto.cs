ed59c2ae86f04e92b31ace508074416472ca9588
Victor.Frts <victorfreitas.job@gmail.com>
2020-11-27 11:25:24 -0300

    Set ItemDto to type record /Dtos

--- Catalog_Net5/Dtos/ItemDto.cs
+++ Catalog_Net5/Dtos/ItemDto.cs
@@ -2,11 +2,11 @@ using System;
 
 namespace Catalog_Net5.Dtos
 {
-    public class ItemDto
+    public record ItemDto
     {
         public Guid Id { get; init; }  //setting init for down allow modifications across the application
         public string Name { get; init; }
         public decimal Price { get; init; }
-        public DateTimeOffset CreatedDate { get; init; }
+        public DateTimeOffset CreatedDate { get; set; }
     }
 }

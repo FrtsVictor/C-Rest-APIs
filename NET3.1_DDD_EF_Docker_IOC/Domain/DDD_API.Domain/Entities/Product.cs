621480573d66eed41cda67b1a9970a4ebcb6ebfe
Victor.Frts <victorfreitas.job@gmail.com>
2020-12-02 21:05:56 -0300

    Add Product /Domain/Entities

---
+++ NET3.1_DDD_EF_Docker_IOC/Domain/DDD_API.Domain/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace DDD_API.Domain.Entities
+{
+    public class Product : Base
+    {
+        public string Name { get; set; }
+        public decimal value { get; set; }
+        public bool IsAvaiable { get; set; }
+    }
+}

efd1b06c68cf311f000ca7560150d51cc3b945a3
Victor.Frts <victorfreitas.job@gmail.com>
2020-12-02 21:05:12 -0300

    Add SqlContext /Infra/Data

---
+++ net3.1_DDD_EF_Docker_IOC/4- Infrastructure/DDD_API.Infrastructure/Data/SqlContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DDD_API.Infrastructure.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext()
+        {
+
+        }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}

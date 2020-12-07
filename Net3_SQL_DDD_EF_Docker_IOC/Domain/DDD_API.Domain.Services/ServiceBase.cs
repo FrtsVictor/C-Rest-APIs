c60f0598ce4935ff16675486073cb8ffe3ad5ed6
Victor.Frts <victorfreitas.job@gmail.com>
2020-12-07 08:47:45 -0300

    Add ServiceBase /Domain/Services

---
+++ Net3_SQL_DDD_EF_Docker_IOC/Domain/DDD_API.Domain.Services/ServiceBase.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using DDD_API.Domain.Core.Interfaces.Repositories;
+using DDD_API.Domain.Core.Interfaces.Services;
+
+namespace DDD_API.Domain.Services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+
+        private readonly IRepositoryBase<TEntity> repository;
+
+        public ServiceBase(IRepositoryBase<TEntity> repository)
+        {
+            this.repository = repository;
+        }
+
+        public void Add(TEntity obj)
+        {
+            repository.Add(obj);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            repository.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            repository.Update(obj);
+        }
+    }
+}

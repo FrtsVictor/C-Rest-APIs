6bb471239e227dbc3d95633ac34c56098907da2e
dnspack <dsnpack@PAT1822.alterdata.matriz>
2020-12-09 23:52:16 -0300

    Add IProductApplication

---
+++ Net3_SQL_DDD_EF_Docker_IOC/Application/DDD_API.Application/Interface/IProductApplication.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Application.DDD_API.Application.Dtos;
+
+namespace Application.DDD_API.Application.Interface
+{
+    public interface IProductApplication
+    {
+         void Add(ProductDto productDto);
+        void Update(ProductDto productDto);
+        void Remove(ProductDto productDto);
+        IEnumerable<ProductDto> GetAll();
+        ProductDto GetById(int id);
+    }
+}

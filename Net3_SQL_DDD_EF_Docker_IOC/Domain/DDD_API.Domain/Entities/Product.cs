3ad19f3594cb7a819c715081cf8b6df36aec0a1c
dnspack <dsnpack@PAT1822.alterdata.matriz>
2020-12-09 23:52:52 -0300

    Refactor prop name

--- Net3_SQL_DDD_EF_Docker_IOC/Domain/DDD_API.Domain/Entities/Product.cs
+++ Net3_SQL_DDD_EF_Docker_IOC/Domain/DDD_API.Domain/Entities/Product.cs
@@ -3,7 +3,7 @@ namespace Domain.DDD_API.Domain.Entities
     public class Product : Base
     {
         public string Name { get; set; }
-        public decimal value { get; set; }
+        public decimal Price { get; set; }
         public bool IsAvaiable { get; set; }
     }
 }

a1bb44a04338e4b4d6d0e5b4f6098e8869411613
FrtsVictor <victorfreitas.job@gmail.com>
2021-03-15 23:49:13 -0300

    Added Migrations

---
+++ Net5_ClientUserApi/src/4 - UserManager.Infra/Migrations/20210315115402_UpdateNameLength.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserManager.Infra.Migrations
+{
+    public partial class UpdateNameLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "User",
+                type: "VARCHAR(75)",
+                maxLength: 75,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(70)",
+                oldMaxLength: 70);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "User",
+                type: "VARCHAR(70)",
+                maxLength: 70,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "VARCHAR(75)",
+                oldMaxLength: 75);
+        }
+    }
+}
